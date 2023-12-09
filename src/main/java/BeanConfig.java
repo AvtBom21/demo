e9ba696d63e567feb14b3b9e367db1f439051db7
AvtBom21 <102849025+AvtBom21@users.noreply.github.com>
2023-12-09 13:39:24 +0700

    Delete src/main directory
    Delete for upload new project that is refix

--- src/main/java/BeanConfig.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@ComponentScan(basePackages = "Model.data")
-@PropertySource("db.properties")
-public class BeanConfig {
-}
